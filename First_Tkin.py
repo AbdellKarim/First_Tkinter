38ce88b00519e8ff4222a3a16203ce23712c925f
abd lkarim <35961636+AbdellKarim@users.noreply.github.com>
2018-01-30 05:26:11 -0800

    Update First_Tkin.py

--- First_Tkin.py
+++ First_Tkin.py
@@ -1,1 +1,29 @@
 
+from tkinter import *
+from tkinter import ttk
+
+
+
+
+class Usrr():
+    
+    def __init__(self, ssl):
+        self.ssl = ssl
+        
+        self.but = Button(ssl, text= "Clicke here")
+        self.but.pack()
+    
+
+
+    def Man(self , ssl):
+        
+        self.sa = ttk.Button(ssl , text = "Hellow")
+        self.sa.pack()
+    
+    
+    
+root = Tk()
+sanp = Usrr(root)
+sanp.Man(root)
+
+root.mainloop()
